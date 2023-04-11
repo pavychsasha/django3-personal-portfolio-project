ecbea190a108f1d41ff1dbc58a763fca4eac152b
pavychsasha <sashpavych@gmail.com>
2023-04-10 22:59:59 -0500

    commiting normal version of the code

--- portfolio/models.py
+++ portfolio/models.py
@@ -2,7 +2,7 @@ from distutils.command.upload import upload
 from django.db import models
 
 class Project(models.Model):
-    titleman = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
     image = models.ImageField(upload_to='portfolio/images/')
     url = models.URLField(blank=True)
